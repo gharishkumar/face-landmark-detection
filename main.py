ad25f5f6b95ec1d43f88a7f613b1055d088db373
Harish Kumar <g.harishkumar1998@gmail.com>
2021-10-22 12:19:12 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture("1.mp4")
+pTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
+drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_TESSELATION, #FACEMESH_TESSELATION #FACEMESH_CONTOURS
+                                    drawSpec,drawSpec)
+            for id,lm in enumerate(faceLms.landmark):
+                #print(lm)
+                ih, iw, ic = img.shape
+                x,y = int(lm.x*iw), int(lm.y*ih)
+                print(id,x,y)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
